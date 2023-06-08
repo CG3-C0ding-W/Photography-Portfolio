10e5793e181c55bb791a8a2d8345cc7b143d4496
CG3-C0ding-W <121372008+CG3-C0ding-W@users.noreply.github.com>
2023-06-08 16:10:31 -0700

    Update Gallery-filter.js

--- Assets/Scripts/Gallery-filter.js
+++ Assets/Scripts/Gallery-filter.js
@@ -1,25 +1,25 @@
-<script>
-    let list = document.querySelectorAll('.list');
-    let pictureBox = document.querySelectorAll('.pictureBox');
+<script>  
+let list = document.querySelectorAll('.list');
+        let totalPictures = document.querySelectorAll('.gallery-Images');
 
-    for (let i = 0; i < list.length; i++) {
-        list[i].addEventListener('click', function() {
-            for (let j = 0; j < list.length; j++) {
-                list[j].classList.remove('active');
-            }
-            this.classList.add('active');
-
-            let dataFilter = this.getAttribute('data-filter');
-
-            for (let k = 0; k < pictureBox.length; k++) {
-                pictureBox[k].classList.remove('active');
-                pictureBox[k].classList.add('hide');
-
-                if (pictureBox[k].getAttribute('data-item') == dataFilter || dataFilter == "all") {
-                    pictureBox[k].classList.remove('hide');
-                    pictureBox[k].classList.add('active');
+        for (let i = 0; i < list.length; i++) {
+            list[i].addEventListener('click', function() {
+                for (let j = 0; j < list.length; j++) {
+                    list[j].classList.remove('active');
                 }
-            }
-        })
-    }
+                this.classList.add('active');
+
+                let dataFilter = this.getAttribute('data-filter');
+
+                for (let k = 0; k < totalPictures.length; k++) {
+                    totalPictures[k].classList.remove('active');
+                    totalPictures[k].classList.add('hide');
+
+                    if (totalPictures[k].getAttribute('data-item') == dataFilter || dataFilter == "all") {
+                        totalPictures[k].classList.remove('hide');
+                        totalPictures[k].classList.add('active');
+                    }
+                }
+            })
+        }
 </script>
