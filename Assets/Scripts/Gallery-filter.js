47f1a257eab255851e55873aff388947d86cf867
CG3-C0ding-W <121372008+CG3-C0ding-W@users.noreply.github.com>
2023-06-08 16:16:21 -0700

    Update Gallery-filter.js

--- Assets/Scripts/Gallery-filter.js
+++ Assets/Scripts/Gallery-filter.js
@@ -1,25 +1,27 @@
 <script>
-    let list = document.querySelectorAll('.list');
-    let totalPictures = document.querySelectorAll('.gallery-Images');
+    function galleryFilter() {
+        let list = document.querySelectorAll('.list');
+        let totalPictures = document.querySelectorAll('.gallery-Images');
 
-    for (let i = 0; i < list.length; i++) {
-        list[i].addEventListener('click', function() {
-            for (let j = 0; j < list.length; j++) {
-                list[j].classList.remove('active');
-            }
-            this.classList.add('active');
-
-            let dataFilter = this.getAttribute('data-filter');
-
-            for (let k = 0; k < totalPictures.length; k++) {
-                totalPictures[k].classList.remove('active');
-                totalPictures[k].classList.add('hide');
-
-                if (totalPictures[k].getAttribute('data-item') == dataFilter || dataFilter == "all") {
-                    totalPictures[k].classList.remove('hide');
-                    totalPictures[k].classList.add('active');
+        for (let i = 0; i < list.length; i++) {
+            list[i].addEventListener('click', function() {
+                for (let j = 0; j < list.length; j++) {
+                    list[j].classList.remove('active');
                 }
-            }
-        })
+                this.classList.add('active');
+
+                let dataFilter = this.getAttribute('data-filter');
+
+                for (let k = 0; k < totalPictures.length; k++) {
+                    totalPictures[k].classList.remove('active');
+                    totalPictures[k].classList.add('hide');
+
+                    if (totalPictures[k].getAttribute('data-item') == dataFilter || dataFilter == "all") {
+                        totalPictures[k].classList.remove('hide');
+                        totalPictures[k].classList.add('active');
+                    }
+                }
+            })
+        }
     }
 </script>
