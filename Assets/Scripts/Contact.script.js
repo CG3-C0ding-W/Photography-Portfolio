46b5407846d875d10cbb0d20b40ae3158e370323
CG3-C0ding-W <121372008+CG3-C0ding-W@users.noreply.github.com>
2023-06-16 20:58:39 -0700

    Update Contact.script.js

--- Assets/Scripts/Contact.script.js
+++ Assets/Scripts/Contact.script.js
@@ -11,3 +11,49 @@
           })
       }
 </script>
+
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+
+var RECEIVER = '<curtis.garrett.iii@gmail.com>';
+var SENDER = '<g3.visuals.photography@gmail.com>';
+
+var response = {
+ "statusCode": 200,
+ "headers": { 
+"Content-Type": "application/json","Access-Control-Allow-Origin": "*"
+},
+"isBase64Encoded": false,
+ "body": "{ \"result\": \"Success\"\n}"
+};
+
+exports.handler = async function (event, context) {
+    console.log('Received event:', event);
+    SendEmailCommand(event, function (err, data) {
+        context.done(err, null);
+    });
+return response;
+};
+
+function sendEmail (event, done) {
+    var params = {
+        Destination: {
+            ToAddresses: [
+                RECEIVER
+            ]
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: 'name: ' + event.name + '\nphone: ' + event.phone + '\nemail: ' + event.email + '\ndesc: ' + event.desc,
+                    Charset: 'UTF-8'
+                }
+            },
+            Subject: {
+                Data: 'G3Visuals Request Form: ' + event.name,
+                Charset: 'UTF-8'
+            }
+        },
+        Source: SENDER
+    };
+    SESClient.SendEmailCommand(params).promise();
+}
