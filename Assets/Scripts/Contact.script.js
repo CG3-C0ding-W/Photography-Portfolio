001c3915cbe6f295f99b4a656722fafa86ce4296
CG3-C0ding-W <121372008+CG3-C0ding-W@users.noreply.github.com>
2023-06-16 21:39:12 -0700

    Update Contact.script.js

--- Assets/Scripts/Contact.script.js
+++ Assets/Scripts/Contact.script.js
@@ -1,59 +1,36 @@
-<script>
-      function SendMail(){
-        var params = {
-          from_name : document.getElementById("fullName").value,
-          email_id: document.getElementById("email_id").value,
-          phone_number: document.getElementById("phone_Number").value,
-          message: document.getElementById("message").value
-        }
-        emailjs.send("service_hgo6ekq", "template_y1sm6aq", params).then(function (res) {
-            alert("Success! " + res.status);
-          })
-      }
-</script>
+const form = document.querySelector("form")
+form.addEventListener("submit", (event) => {
+  // prevent the form submit from refreshing the page
+  event.preventDefault();
 
-import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+  const { name, phone, email, message } = event.target;
 
-var RECEIVER = '<curtis.garrett.iii@gmail.com>';
-var SENDER = '<g3.visuals.photography@gmail.com>';
+  // Use your API endpoint URL you copied from the previous step
+  const endpoint = "<https://1i8bgk2qm4.execute-api.us-west-1.amazonaws.com/default/contactSubmission>";
+  
+ // We use JSON.stringify here so the data can be sent as a string via HTTP
+    const body = JSON.stringify({
+    senderName: document.getElementById("fullName ").value,
+    senderPhone: document.getElementById("phone_Number ").value,
+    senderEmail: document.getElementById("email_id ").value,
+    message: document.getElementById("message ").value
+  });
+  const requestOptions = {
+    method: "POST",
+    body
+  };
 
-var response = {
- "statusCode": 200,
- "headers": { 
-"Content-Type": "application/json","Access-Control-Allow-Origin": "*"
-},
-"isBase64Encoded": false,
- "body": "{ \"result\": \"Success\"\n}"
-};
-
-exports.handler = async function (event, context) {
-    console.log('Received event:', event);
-    SendEmailCommand(event, function (err, data) {
-        context.done(err, null);
+  fetch(endpoint, requestOptions)
+    .then((response) => {
+      if (!response.ok) throw new Error("Error in fetch");
+      return response.json();
+    })
+    .then((response) => {
+      document.getElementById("result-text").innerText =
+        "Email sent successfully!";
+    })
+    .catch((error) => {
+      document.getElementById("result-text").innerText =
+        "An unkown error occured.";
     });
-return response;
-};
-
-function sendEmail (event, done) {
-    var params = {
-        Destination: {
-            ToAddresses: [
-                RECEIVER
-            ]
-        },
-        Message: {
-            Body: {
-                Text: {
-                    Data: 'name: ' + event.name + '\nphone: ' + event.phone + '\nemail: ' + event.email + '\ndesc: ' + event.desc,
-                    Charset: 'UTF-8'
-                }
-            },
-            Subject: {
-                Data: 'G3Visuals Request Form: ' + event.name,
-                Charset: 'UTF-8'
-            }
-        },
-        Source: SENDER
-    };
-    SESClient.SendEmailCommand(params).promise();
-}
+});
